d8ade463c07c5362b97e2c2c7adc83c849b05618
brainyfarm <brainyfarm@gmail.com>
2016-08-23 04:53:34 +0100

    Code cleanup

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const readline = require('readline');
 
 //Set up readline module
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 // Import request
@@ -16,58 +16,66 @@ const request = require('request');
 
 // API and Token keys
 const client = new Twitter({
-  consumer_key: 'ZZ6rdrhIgwDpTh1V9DNzNk6Y2',
-  consumer_secret: 'PG1Eiv7yBZeKDqiiwKLVGcftr1a4hSKLdA7sbqOArQ5x484hqa',
-  access_token_key: '445679779-iUcgExLGGxSg4cEZmD6WSkT6NR7QN4QAQQhUQjZP',
-  access_token_secret: 'kQLK8PJqCYEAgslLgmnXfDUI9UPD6d9LgJMGCkBSA7uC4'
+    consumer_key: 'ZZ6rdrhIgwDpTh1V9DNzNk6Y2',
+    consumer_secret: 'PG1Eiv7yBZeKDqiiwKLVGcftr1a4hSKLdA7sbqOArQ5x484hqa',
+    access_token_key: '445679779-iUcgExLGGxSg4cEZmD6WSkT6NR7QN4QAQQhUQjZP',
+    access_token_secret: 'kQLK8PJqCYEAgslLgmnXfDUI9UPD6d9LgJMGCkBSA7uC4'
 });
 
 
 
 const quoteApi = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
-console.log("Please wait while I fetch a quote\n\n")
-// Get a random quote
+console.log("Please wait while I fetch a quote\n\n");
 
-function coolQuote() {
-request(quoteApi, function (error, response, body) {
+/**
+ * getQuote 
+ * Gets a random quote and posts to twitter
+ */
+function getQuote() {
 
-// If request was successful
-  if (!error && response.statusCode == 200) {
-    let randomQuote = JSON.parse(body);
+    request(quoteApi, function(error, response, body) {
 
-    let text = randomQuote.quoteText
-    let author = randomQuote.quoteAuthor;
-    toTweet = text + " by " + author; 
+        // If getting a new quote succeeds
+        if (!error && response.statusCode === 200) {
+            let randomQuote = JSON.parse(body);
 
-    console.log("\n  " +text);
+            let text = randomQuote.quoteText
+            let author = randomQuote.quoteAuthor;
+            toTweet = text + " -" + author;
 
-  // Ask if user would like to post this
-  rl.question('\n\nType "yes" to tweet, "no" to start over\n' , (userChoice) => {
-    // Tweet or get new quote based on user's choice
-    if(userChoice.toLowerCase() == "yes")
-      tweet(toTweet);
-    // If user choose not to post
-    if(userChoice.toLowerCase() == "no")
-      // get a new quote
-      coolQuote();
-    
-    rl.close();
- });
+            console.log("\n  " + toTweet);
 
-  }
-})
+            // Ask if user would like to post this
+            rl.question('\n\nType "yes" to tweet, `CTRL + C` to exit\n', (userChoice) => {
+                
+                // If user decides to tweet
+                if (userChoice.toLowerCase() == "yes")
+                    tweet(toTweet);
+                
+                rl.close();
+            });
+
+        }
+    })
 }
 
-function tweet(tweetText){
-  // Post it to twitter
-  client.post('statuses/update', {status: toTweet},  function(error, tweet, response) {
-    if(error) throw error;
+/**
+* tweet
+* Posts some texts to twitter 
+* @param tweetText
+*/
+function tweet(tweetText) {
+    // Post it to twitter
+    client.post('statuses/update', {
+        status: toTweet
+    }, function(error, tweet, response) {
+        if (error) console.log("Unable to tweet");
 
-        console.log("Tweet posted!");  
-      });
+        console.log("Tweet posted!");
+    });
 
- }
+}
 
-
- coolQuote();
+// Get a quote
+getQuote();
